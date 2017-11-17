b002f7165002c43675134c9c6fda3fcead498859
thefredo1000 <popobasura3@hotmail.com>
2017-11-17 15:30:00 -0600

    Initial Commit

---
+++ Mis Calis/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mis Calis
+//
+//  Created by Rodrigo Casale on 11/17/17.
+//  Copyright © 2017 Rodrigo Casale. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
